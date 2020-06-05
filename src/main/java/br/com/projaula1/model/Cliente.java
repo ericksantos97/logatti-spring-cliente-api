c0365bdf53ceea71a2830f398e5f638e9deb928c
Erick Santos <erick.tec97@gmail.com>
2020-06-05 19:52:45 -0300

    include lombok

--- src/main/java/br/com/projaula1/model/Cliente.java
+++ src/main/java/br/com/projaula1/model/Cliente.java
@@ -1,39 +1,22 @@
 package br.com.projaula1.model;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
+@Getter
+@Setter
 public class Cliente {
 
     @Id
     private int id;
+    private String cpf;
     private String nome;
     private int telefone;
 
-    public int getId() {
-        return id;
-    }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(int telefone) {
-        this.telefone = telefone;
-    }
 
 
 }
--- src/main/java/br/com/projaula1/model/Produto.java
+++ src/main/java/br/com/projaula1/model/Produto.java
@@ -1,28 +1,18 @@
 package br.com.projaula1.model;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
+@Getter
+@Setter
 public class Produto {
 
     @Id
     private int id;
     private String nome;
 
-    public int getId() {
-        return id;
-    }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
 }
